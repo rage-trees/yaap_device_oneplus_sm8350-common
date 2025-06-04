7a6b6d3f8942b6821c5fcd4777ddcef94c5eff3a
Eduardo H. <3580681+THERAD1@users.noreply.github.com>
2025-06-04 13:31:38 -0400

    common: overlay path updates

--- common.mk
+++ common.mk
@@ -218,6 +218,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-yaap
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
